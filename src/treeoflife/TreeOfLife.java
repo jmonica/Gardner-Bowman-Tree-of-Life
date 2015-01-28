3637e46e28b377b3ae86fb45bfbb420d1fdc1357
MonicasApple <MonicasApple@192.168.5.184>
2015-01-28 12:38:51 -0700

    Initial commit of project

---
+++ src/treeoflife/TreeOfLife.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treeoflife;
+
+import byui.cit260.treeOfLife.model.Game;
+
+/**
+ *
+ * @author MonicasApple
+ */
+public class TreeOfLife {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Game gameOne = new Game();
+        
+        gameOne.setTotalTime(7.00);
+        
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo);
+        
+    }
+    
+}
