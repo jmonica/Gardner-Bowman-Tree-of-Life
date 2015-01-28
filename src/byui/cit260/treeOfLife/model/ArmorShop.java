7c023ebbda73b36e39781acc3fb8a5314f2c34a7
gradygb <gradygb@Bowman>
2015-01-28 16:06:27 -0700

    initial armor shop

---
+++ src/byui/cit260/treeOfLife/model/ArmorShop.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.treeOfLife.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author gradygb
+ */
+public class ArmorShop implements Serializable{
+    
+    private String inventoryType;
+    private int quantityInStock;
+    private int requiredAmount;
+
+    public ArmorShop() {
+    }
+
+    public String getInventoryType() {
+        return inventoryType;
+    }
+
+    public void setInventoryType(String inventoryType) {
+        this.inventoryType = inventoryType;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public int getRequiredAmount() {
+        return requiredAmount;
+    }
+
+    public void setRequiredAmount(int requiredAmount) {
+        this.requiredAmount = requiredAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "ArmorShop{" + "inventoryType=" + inventoryType + ", quantityInStock=" + quantityInStock + ", requiredAmount=" + requiredAmount + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.inventoryType);
+        hash = 97 * hash + this.quantityInStock;
+        hash = 97 * hash + this.requiredAmount;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ArmorShop other = (ArmorShop) obj;
+        if (!Objects.equals(this.inventoryType, other.inventoryType)) {
+            return false;
+        }
+        if (this.quantityInStock != other.quantityInStock) {
+            return false;
+        }
+        if (this.requiredAmount != other.requiredAmount) {
+            return false;
+        }
+        return true;
+    }
+    
+            
+    
+}
