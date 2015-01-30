3af6bc01cbc53d46d6218960a7ad02da247e46ba
gradygb <gradygb@Bowman>
2015-01-30 10:21:40 -0700

    initial armor shop

--- src/treeoflife/TreeOfLife.java
+++ src/treeoflife/TreeOfLife.java
@@ -5,7 +5,11 @@
  */
 package treeoflife;
 
+import byui.cit260.treeOfLife.model.ArmorShop;
 import byui.cit260.treeOfLife.model.Game;
+import byui.cit260.treeOfLife.model.Inventory;
+import byui.cit260.treeOfLife.model.PersonalInventory;
+import byui.cit260.treeOfLife.model.Scene;
 
 /**
  *
@@ -23,7 +27,42 @@ public class TreeOfLife {
         
         String gameInfo = gameOne.toString();
         System.out.println(gameInfo);
-        
-    }
     
+        Scene sceneOne = new Scene();
+        
+        sceneOne.setType('a');
+        sceneOne.setDescription("This is a forest");
+        sceneOne.setSymbol('b');
+        sceneOne.setBlocked('c');
+        
+        String sceneInfo = sceneOne.toString();
+        System.out.println(sceneInfo);
+        
+        ArmorShop armorShop = new ArmorShop();
+        
+        armorShop.setInventoryType("Inventory type is armor");
+        armorShop.setQuantityInStock(5);
+        armorShop.setRequiredAmount(8);
+        
+        String armorInfo = armorShop.toString();
+        System.out.println(armorInfo);
+        
+        Inventory inventoryOne = new Inventory();
+        
+        inventoryOne.setInventoryType("Inventory type is a sword");
+        inventoryOne.setQuantityInStock(1);
+        inventoryOne.setRequiredAmount(3);
+        
+        String inventoryInfo = inventoryOne.toString();
+        System.out.println(inventoryInfo);
+        
+        PersonalInventory personalOne = new PersonalInventory();
+        
+        personalOne.setInventoryType("This is personal inventory");
+        personalOne.setMonyAmount(4);
+        personalOne.setQuantityInStock(7);
+        
+        String personalInfo = personalOne.toString();
+        System.out.println(personalInfo);
+        }
 }
