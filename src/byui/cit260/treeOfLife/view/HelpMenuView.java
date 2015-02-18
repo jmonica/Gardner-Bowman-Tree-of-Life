6c2cc324c8d8997162b812a2012f39e23e172edc
gradygb <gradygb@Bowman>
2015-02-17 17:21:58 -0700

    Merge origin/master
    Conflicts:
    	src/byui/cit260/treeOfLife/control/GameControl.java
    	src/byui/cit260/treeOfLife/view/GameMenuView.java
    	src/byui/cit260/treeOfLife/view/MainMenuView.java

--- src/byui/cit260/treeOfLife/view/HelpMenuView.java
+++ src/byui/cit260/treeOfLife/view/HelpMenuView.java
@@ -11,8 +11,76 @@ package byui.cit260.treeOfLife.view;
  */
 public class HelpMenuView {
 
-    void displayMenu() {
-        System.out.println("*** HelpMenuView.displayMenu() function called ***");
-    }
+    private final String MENU = "\n"
+            +"\n----------------------------------------------------"
+            +"\n| Help Menu                                        |"
+            +"\n----------------------------------------------------"
+            +"\nG - What is the goal of the game?"
+            +"\nM - How to move?"
+            +"\nQ - Quit"
+            +"\n----------------------------------------------------";
     
+    public void displayMenu() {
+        
+        char selection = ' ';
+        do {
+        
+        System.out.println(MENU); //display the help menu
+        
+        String input = this.getInput(); //get the usesr's selection
+        selection = input.charAt(0); // get firt character of string
+        
+        this.doAction(selection); // do action based on selection
+        
+        } while (selection != 'Q'); // an selection is not "Quit"
+    }
+
+      public String getInput() {
+        boolean valid = false;
+        String getInput = null;
+        Scanner keyboard = new Scanner(System.in);
+        
+        while(!valid){
+        
+        //prompt for the selection
+        System.out.println("Choose your selection");
+        
+        //get the selection from the keyboard and trim off the blanks
+        getInput = keyboard.nextLine();
+        getInput = getInput.trim();
+        
+        //if the selection is invaled 
+        if (getInput.length() != 1){
+            System.out.println("Invalid selection");
+            continue;
+        }
+        break;
+        }
+        return getInput;
+    }
+
+    private void doAction(char selection) {
+        
+        switch (selection) {
+            case 'G':
+                this.gameInstruction();
+                break;
+            case 'M':
+                this.howToMove();
+                break;
+            case 'Q':
+                return;
+            default:
+                System.out.println("\n*** Invalid selection ***");
+                break;
+        }
+    }
+
+    private void gameInstruction() {
+        System.out.println("*** gameInstruction function called ***");
+    }
+
+    private void howToMove() {
+        System.out.println("*** gameInstruction function called ***");
+    }
 }
