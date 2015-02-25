6b875e083b17dd49b0e6e22bfc25049bc0d01d05
MonicasApple <MonicasApple@192.168.5.184>
2015-02-25 09:39:35 -0700

    Monica Commit Jan 30

--- src/byui/cit260/treeOfLife/view/GameMenuView.java
+++ src/byui/cit260/treeOfLife/view/GameMenuView.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package byui.cit260.treeOfLife.view;
 
 import java.util.Scanner;
 
-/**
- *
- * @author gradygb
- */
 public class GameMenuView {
     private final String MENU = "\n"
             +"\n----------------------------------------------------"
